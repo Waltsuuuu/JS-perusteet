24b7c7fb1ba4bdb7c25e652981be0910c35d205e
Waltsuuu <waltteriheino@gmail.com>
2024-04-08 20:22:02 +0300

    Finished assignment merkkijonot_03

---
+++ Osa2/06_Merkkijonot/merkkijonot_03.js
@@ -0,0 +1,24 @@
+/**
+Muokkaa funktiota lowerName, jotta se palauttaa saamansa name-parametrin kokonaan pienin kirjaimin
+
+ */
+function lowerName(name) {
+    return name.toLowerCase();
+}
+
+// Esimerkki - älä muokkaa
+console.log(lowerName('Sam')); // "sam"
+console.log(lowerName('ALEX')); // "alex"
+
+
+
+// Treenaa: Keksi itse samanlainen funktio eri nimellä
+
+whisperMyName = (name) => {
+    return name.toLowerCase();
+}
+
+
+// Kutsu tekemääsi funktiota
+
+console.log(whisperMyName("WALTTERI"));
