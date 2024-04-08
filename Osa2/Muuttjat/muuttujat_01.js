00dcfcee144b5d6f2313fb891b0279cd3eb87e62
Waltsuuu <waltteriheino@gmail.com>
2024-04-08 12:30:23 +0300

    Muuttujat tehtävät valmiit

---
+++ Osa2/Muuttjat/muuttujat_01.js
@@ -0,0 +1,6 @@
+let count = 0;
+console.log("Muuttujan arvo alussa:" + count)
+count++;
+console.log("Muuttujan arvo välissä:" + count)
+count = count + 5;
+console.log("Muuttujan arvo lopussa:" + count)
