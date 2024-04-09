6acd83c0771bfe00c32aef6ee67d0f01629d0992
Waltsuuu <waltteriheino@gmail.com>
2024-04-09 14:18:09 +0300

    Finished assignment ehtolause_06

---
+++ Osa3/03_harjoitukset_ehtolauseet/ehtolause_06.js
@@ -0,0 +1,19 @@
+/**
+Kehitä funktio nimeltä greaterNum, joka:
+
+- Hyväksyy kaksi argumenttia, jotka molemmat ovat numeroita.
+- Palauttaa näistä kahdesta suuremman numeron.
+ */
+
+function greaterNum(num1, num2) {
+    if (num1 > num2) {
+        return (`${num1}`)
+    } else {
+        return (`${num2}`)
+    }
+}
+
+// Esimerkkikäyttö - älä muokkaa
+console.log(greaterNum(5, 10));
+console.log(greaterNum(2, 1));
+console.log(greaterNum(4, 25));
