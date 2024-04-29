2000315b3213032609de7f87ab8860bfee988751
Waltsuuu <waltteriheino@gmail.com>
2024-04-29 21:40:58 +0300

    Added usrname and email verification. Edited order summary section. Assignment not finished

--- Osa6/PancakeTilaus2/script.js
+++ Osa6/PancakeTilaus2/script.js
@@ -1,18 +1,21 @@
-//selects the first element with class "form-container"
 const order = document.querySelector(".form-container");
-
-let userName = document.getElementById('nameInput');
-changeUsername = () => {
-  userName = document.getElementById('nameInput').value;
-}
+//Global variable for pancake type
 const type = document.querySelector("#type");
+let userName = document.getElementById("nameInput");
+let userEmail = document.getElementById("emailInput");
 
+changeUsername = () => {
+  userName = document.getElementById("nameInput").value;
+};
 
-//Function that calculates and updates total price.
+changeEmail = () => {
+  userEmail = document.getElementById("emailInput").value;
+};
+
+//CALCULATES AND UPDATES USER ON ORDER PRICE.
 const priceCalculator = () => {
-  //pannukakku hinta
+  //Pancake price
   let typePrice = Number(type.value);
-
   console.log(`Pannukakku = ${typePrice}€`);
 
   //pannukakun tyyppi for summary
@@ -59,7 +62,7 @@ const priceCalculator = () => {
   }
   console.log(extraArray);
 
-  let userName = document.getElementById('nameInput').value;
+ // let userName = document.getElementById("nameInput").value;
   console.log(userName);
 
   //kokonaishinta
@@ -71,54 +74,82 @@ const priceCalculator = () => {
   let hintaNäyttö = document.getElementsByClassName("totalPrice");
 
   console.log(hintaNäyttö);
-  
+
   //Turns collection into array
   hintaNäyttöArr = Array.from(hintaNäyttö);
 
   //Runs function on each array element, function changes the elements innerHTML to variable price.
-  hintaNäyttöArr.forEach((element) => element.innerHTML = (`${price}€`));
+  hintaNäyttöArr.forEach((element) => (element.innerHTML = `${price}€`));
 };
 
+//Re-reveals hidden elements if user decides to go back to change order
 const returnToOrder = () => {
-  document.getElementById('orderSummary').style.display = 'none';
+  document.getElementById("orderSummary").style.display = "none";
   order.style.display = "";
-  document.getElementById('submitOrder').style.display = "";
-}
-
+  document.getElementById("submitOrder").style.display = "";
+  document.getElementById("lisukeLi").style.display = "";
+  document.getElementById("extraLi").style.display = "";
+};
 
 //SUBMITTING ORDER
 const submitOrder = () => {
-//Stops user from advancing if they have no entered their name.
-  if (document.getElementById('nameInput').value == ""){
+  //Stops user from advancing if they have no entered their name or email
+  if (document.getElementById("nameInput").value == "") {
     window.alert("Lisää tilaajan nimi!");
-    return
-  }
+    return;
+  };
 
-//Hides order form and reveals order summary section
-  document.getElementById('orderSummary').style.display = 'flex';
+  if (document.getElementById('emailInput').value == "") {
+    window.alert("Enter email");
+    return;
+  };
+
+  //Makes sure user email is correctly formatted
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (userEmail.match(mailformat)){
+  } else {
+    window.alert("You have entered an invalid email address!");
+    return;
+  };
+
+  //Hides order form and reveals order summary section
+  document.getElementById("orderSummary").style.display = "flex";
   order.style.display = "none";
-  document.getElementById('submitOrder').style.display = "none";
+  document.getElementById("submitOrder").style.display = "none";
 
-  document.getElementById('namePlaceholder').textContent = userName;
-  document.getElementById('typePlaceholder').innerHTML = typeSummary;
-  document.getElementById('lisukePlaceholder').textContent = lisukeArray.join(', ');
-  document.getElementById('extraPlaceholder').textContent = extraArray.join(', ');
-  document.getElementById('pricePlaceholder').textContent = `${price}€`;
+  //Gets users order details and lists them in summary
+  document.getElementById("namePlaceholder").textContent = userName;
+  document.getElementById("emailPlaceholder").textContent = userEmail;
+  document.getElementById("typePlaceholder").innerHTML = typeSummary;
+  
+  //If user doesnt select lisukkeet, lisukkeet listitem hidden. 
+  //If lisukkeet selected, display lisukkeet array
+  if (lisukeArray.length <= 0) {
+    document.getElementById("lisukeLi").style.display = "none";
+  } else {  
+  document.getElementById("lisukePlaceholder").textContent =
+    lisukeArray.join(", ")
+  };
+
+  if (extraArray.length <= 0) {
+    document.getElementById("extraLi").style.display = "none";
+  } else {
+  document.getElementById("extraPlaceholder").textContent =
+    extraArray.join(", ");
+  }
+  document.getElementById("pricePlaceholder").textContent = `${price}€`;
 }
 
 
-
-//EVENTLISTNERS
+//EVENTLISTENERS
 
 //when -change- happens in variable "order", run function priceCalculator.
 order.addEventListener("change", priceCalculator);
 //when submitOrder is clicked, run function submitOrder.
-document.getElementById('submitOrder').addEventListener('click', submitOrder);
+document.getElementById("submitOrder").addEventListener("click", submitOrder);
 //when username text field is changed, run function changeUsername
-userName.addEventListener('change', changeUsername);
+userName.addEventListener("change", changeUsername);
+//when userEmail textfield is change, run function changeEmil
+userEmail.addEventListener("change", changeEmail);
 //when user clicks 'palaa muokkaamaan tilausta on the summary page, returns user to order page.
-document.getElementById('muokkaaTilausta').addEventListener('click', returnToOrder);
-
-
-
-
+document.getElementById("muokkaaTilausta").addEventListener("click", returnToOrder);
